690d7208ab4905029b416f9daa831220c7dc4fe3
Anwesh43 <anweshthecool0@gmail.com>
2018-05-25 08:18:18 +0530

    adding app module

---
+++ app/src/main/java/com/example/anweshmishra/kotlinlinkedpointsidewiseview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.anweshmishra.kotlinlinkedpointsidewiseview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
