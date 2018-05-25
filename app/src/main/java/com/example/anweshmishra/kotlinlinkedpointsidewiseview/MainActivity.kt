db8b58324cde58148848a0d7b1db78f5d67dfeec
Anwesh43 <anweshthecool0@gmail.com>
2018-05-25 10:10:32 +0530

    adding fullscreen support

--- app/src/main/java/com/example/anweshmishra/kotlinlinkedpointsidewiseview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinlinkedpointsidewiseview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinlinkedpointsidewiseview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.linkedpointsidewiseview.PointSideWiseView
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PointSideWiseView.create(this)
-
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
