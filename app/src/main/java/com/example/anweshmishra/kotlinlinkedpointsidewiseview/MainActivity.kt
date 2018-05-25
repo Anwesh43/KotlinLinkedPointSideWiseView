65988b36a2e18cac8dc4798d73f7051c61187a21
Anwesh43 <anweshthecool0@gmail.com>
2018-05-25 10:04:21 +0530

    adding View in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinlinkedpointsidewiseview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinlinkedpointsidewiseview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.anweshmishra.kotlinlinkedpointsidewiseview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.linkedpointsidewiseview.PointSideWiseView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PointSideWiseView.create(this)
 
     }
 }
